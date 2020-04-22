7dbe855095ac9d5ffd8881bedb956faa653bc569
liuyashao <liuyashao@zysoftware.com>
2020-04-22 16:47:47 +0800

    1

--- temp project/sql/create table sto_Stock.sql
+++ temp project/sql/create table sto_Stock.sql
@@ -1,6 +1,6 @@
 create table sto_Stock(
 Id INT NOT NULL PRIMARY KEY,
-StoreNo varchar(20) not null,
+WHCode varchar(10) not null,
 GoodsCode varchar(50) not null,
 QtyY decimal(18, 2) not null default 0;
 SpecY decimal(18, 2) not null, 
@@ -8,17 +8,17 @@ Lot varchar(30) default '',
 Seat varchar(30) default '',
 CostPrice decimal(18, 4) not null default 0,
 LastInDate timestamp,
-LastSaleDate timestamp
+LastOutDate timestamp
 )
+
+
+create index idx_sto_Stock_WHCode on sto_Stock(WHCode, GoodsCode, SpecY, Lot, Seat) 
 go
 
-create index idx_sto_Stock_GoodsCode on sto_Stock(GoodsCode, StoreNo) 
+create index idx_sto_Stock_GoodsCode on sto_Stock(GoodsCode, WHCode) 
 go
 
-create index idx_sto_Stock_StoreNo on sto_Stock(StoreNo, GoodsCode, SpecY, Lot, Seat)
-go
-
-create index idx_sto_Stock_Qty on sto_Stock(Qty)
+create index idx_sto_Stock_SpecY on sto_Stock(SpecY)
 go
 
 create index idx_sto_Stock_Lot on sto_Stock(Lot)
