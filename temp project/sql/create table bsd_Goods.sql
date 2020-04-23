16bde76646e658310cebcb365235bd5028f0e6d1
lys <liuyashao@foxmail.com>
2020-04-23 22:16:04 +0800

    1

--- temp project/sql/create table bsd_Goods.sql
+++ temp project/sql/create table bsd_Goods.sql
@@ -1,5 +1,4 @@
 create table bsd_Goods(
-Id int not null,
 GoodsCode varchar(50) not null primary key,
 GoodsName varchar(100) not null,
 GoodsGroup varchar(100) not null,
@@ -14,8 +13,8 @@ SpecM decimal(12, 2) not null,
 SpecSaleY decimal(12, 2) not null,
 SpecSaleM decimal(12, 2) not null,
 
-pfPrice decimal(12, 2),
-lsPrice decimal(12, 2),
+SalePrice decimal(12, 2),
+WholPrice decimal(12, 2),
 MinPrice decimal(12, 2),
 
 BottomCloth varchar(100),
@@ -23,10 +22,9 @@ Grain varchar(100),
 Thick varchar(100),
 Size varchar(100),
 
-DefaultltSeat varchar(30),
+DefSeat varchar(30),
 FactoryCode varchar(50),
-SupplierCode varchar(50),
-SupplierName varchar(100),
+DefSupplierCode varchar(20),
 
 Disabled int default 0,
 
--- temp project/sql/create table bsd_GoodsGroup.sql
+++ temp project/sql/create table bsd_GoodsGroup.sql
@@ -1,4 +1,5 @@
 create table bsd_GoodsGroup(
-GoodsGroup varchar(100) not null primary key
+GoodsGroup varchar(100) not null primary key,
+PYCode varchar(100)
 )
 go
