60956d3b6d1130f20e53fb1f3df4079ec80a5ef2
liuyashao <liuyashao@zysoftware.com>
2020-04-24 17:07:04 +0800

    1

--- temp project/utils/uClasses.pas
+++ temp project/utils/uClasses.pas
@@ -11,7 +11,7 @@ type
   TCostManageTypeHelper = record helper of TCostManageType
     function ToString: string;
 	function ToInteger: Integer;
-	class fucntion P(const Str: string): TCostManageType; static;
+	class function Parse(const Str: string): TCostManageType; static;
   end
   
   TWHProperty = record
@@ -50,12 +50,51 @@ type
     SupplierName: string;
     Disabled: Boolean;
     PYCode: string;
-	class function LoadFromDB(const WHCode: string; Conn: TUniConn): TWHProperty; static;
+	class function LoadFromDB(const GoodsCode: string; Conn: TUniConn): TWHProperty; static;
   end;
   
  implementation
  
  uses
    uFunc;
+   
+{ TCostManageTypeHelper }
+   
+function TCostManageTypeHelper.ToString: string;
+begin
+  case Self of
+    cmtMovingAverage: Result := '移动平均';
+	cmtLot: Result := '批次计价';
+  end;	
+end;
+
+function TCostManageTypeHelper.ToInteger: Integer;
+begin
+  Result := Ord(Self):
+end;
+
+class function TCostManageTypeHelper.Parse(const Str: string): TCostManageType;
+begin
+  if SameTextEx(Str, ['移动平均', cmtMovingAverage.ToInteger.ToString]) then
+    Result := cmtMovingAverage else
+  if SameTextEx(Str, ['批次计价', cmtLot.ToInteger.ToString]) then
+    Result := cmtLot
+  else
+    raise Exception.CreateFmt('can not Parse %s to TCostManageType type', [Str]);
+end;
+
+{ TWHProperty }
+
+class function TWHProperty.LoadFromDB(const WHCode: string; Conn: TUniConn = nil): TWHProperty;
+begin
+
+end;
+
+{ TGoodsProperty }
+
+class function TGoodsProperty.LoadFromDB(const GoodsCode: string; Conn: TUniConn): TWHProperty;
+begin
+
+end;
   
- end.
+end.
