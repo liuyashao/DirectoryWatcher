438e71282044f0e506b694a9066e17d5194cd9cf
lys <liuyashao@foxmail.com>
2020-04-28 21:16:58 +0800

    1

--- temp project/sql/create table bsd_store.sql
+++ temp project/sql/create table bsd_store.sql
@@ -2,8 +2,8 @@ create table bsd_Store(
 StoreNo varchar(10) not null,
 Name varchar(100) not null,
 Id int not null,
-LinkPerson varchar(20)
-Telphone varchar(100) null,
+LinkPerson varchar(20),
+Telphone varchar(100),
 AddressProvince varchar(20),
 AddressCity varchar(20),
 AddressCounty varchar(20),
@@ -11,6 +11,7 @@ AddressTown varchar(20),
 AddressDetail varchar(100),
 CONSTRAINT PK_bsd_Store PRIMARY KEY (StoreNo)
 )
+
 go
 
 create unique index idx_bsd_Store_Id on bsd_Store(Id)
